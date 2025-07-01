b6e5b7849bd290debeaed85acc82f11a04029fb3
I551393 <florian.veitz@sap.com>
2025-07-01 12:26:40 +0200

    initialized

---
+++ skripts/forecasting/models.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+class Model:
+    df = pd.DataFrame()
+
+    def __init__(self, df):
+        self.df = df
+
+    def base_line(self) -> pd.DataFrame:
+        # ToDo: Ksenia (last value)
+        return None
+
+    def auto_arima(self) -> pd.DataFrame:
+        # ToDo Flo (auto arima from pmdarima)
+        return None
+    
+    def LSTM(self) -> pd.DataFrame:
+        # ToDo Ksenia
+        return None
+    
+    def prophet(self) -> pd.DataFrame:
+        # ToDo Flo
+        return None
+    
+    def times_fm(self) -> pd.DataFrame:
+        # ToDo Ksenia
+        return None
+    
+    def time_gpt(self) -> pd.DataFrame:
+        # ToDo Flo
+        return None
+    
+# Datasets Flo: 1.1 & 1.3
+
+# Datasets Ksenia: 1.2 & 1.4
